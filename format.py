5f8efa855b593f179f194e0edf7d142b3f6a79b8
Caden James Witzeman <95098418+jameswitzeman@users.noreply.github.com>
2024-01-26 15:22:53 -0700

    Update format.py
    Fixed filename

--- format.py
+++ format.py
@@ -2,7 +2,7 @@ with open('GHISACONUS_2008_001_speclib.csv', 'r') as ghisar:
     lines = ghisar.readlines()
     ptr = 1
 
-    with open('GHISACONUS_2008_speclib.csv', 'w') as ghisaw:
+    with open('GHISACONUS_2008_001speclib.csv', 'w') as ghisaw:
         for line in lines:
             if ptr > 6:
                 ghisaw.write(line)
