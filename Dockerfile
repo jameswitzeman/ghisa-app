abfdc2388b8cef7c65576e1a7eb3dc51f50c09c1
Caden James Witzeman <95098418+jameswitzeman@users.noreply.github.com>
2024-02-05 15:39:10 -0700

    Almost all data formatted in this build

--- Dockerfile
+++ Dockerfile
@@ -14,6 +14,7 @@ RUN pip install install conda
 #Download and setup LPDAAC (WIP)
 WORKDIR /LPDAAC
 COPY ./format.py .
+COPY ./tag-delete.py .
 
   #Get the download script from NASA
 RUN wget -O DAACDataDownload.py https://git.earthdata.nasa.gov/projects/LPDUR/repos/daac_data_download_python/raw/DAACDataDownload.py?at=refs%2Fheads%2Fmain
@@ -25,10 +26,8 @@ RUN chmod 555 DAACDataDownload.py
 COPY downloads.txt .
 COPY netrc /home/jovyan/.netrc
 RUN python DAACDataDownload.py -dir . -f downloads.txt
-RUN rm .netrc
+RUN rm /home/jovyan/.netrc
 
-#Format the data docs
-RUN python format.py
 
 #GHISA pip dependencies
 WORKDIR /home/jovyan
@@ -42,13 +41,20 @@ RUN pip install --upgrade hvplot
 #Get the actual GHISA app, make it read/execute only for visitors
 WORKDIR /
 RUN git clone https://github.com/jameswitzeman/GHISA_Spectral_Visualization_App/
-RUN mv desis_spectral_library_data_release.csv /LPDAAC
-RUN mv prisma_spectral_library_data_release.csv /LPDAAC
+
+WORKDIR /LPDAAC
+#Format the data docs
+RUN wget -O prisma_data.csv https://www.sciencebase.gov/catalog/file/get/62a91cc2d34ec53d2770f06d?f=__disk__cb%2F45%2F62%2Fcb4562caa9aaad6b4c55db13925f24b09aa7dbd3
+RUN wget -O desis_data.csv https://www.sciencebase.gov/catalog/file/get/62a91cc2d34ec53d2770f06d?f=__disk__d2%2F06%2Fe4%2Fd206e4e55fd1f545bd204e0475fcc7b5b7c9410d
+
+RUN python /LPDAAC/tag-delete.py
+RUN python /LPDAAC/format.py
+
+RUN rm desis_data.py && rm prisma_data.py
 
 WORKDIR /work
 RUN mv /GHISA_Spectral_Visualization_App/GHISA_Visualization.ipynb .
 
 #MAKE SURE THIS IS 555 IN PROD
 RUN chmod 777 GHISA_Visualization.ipynb
-RUN chmod 777 /home/joyvan/work/GHISA_Visualization.ipynb
 
--- format.py
+++ format.py
@@ -1,9 +1,115 @@
-with open('GHISACONUS_2008_001_speclib.csv', 'r') as ghisar:
-    lines = ghisar.readlines()
-    ptr = 1
+#Formatting
+import pandas as pd
+import numpy as np
+import geopy
+import geemap
+import ee
+import config
+import jinja2
+import hvplot.pandas
+import holoviews as hv
+import geopandas as gpd
+import geoviews as gv
+import panel as pn
 
-    with open('GHISACONUS_2008_001_speclib.csv', 'w') as ghisaw:
-        for line in lines:
-            if ptr > 6:
-                ghisaw.write(line)
-            ptr += 1
+print("Loading Hyperion data...")
+
+#Begin importing data
+
+  #GHISA-CONUS-2008 data
+core_data = pd.read_csv(r'/LPDAAC/GHISACONUS_2008_001_speclib.csv',
+                     low_memory=False, index_col=False, na_values='NaN') 
+  #GHISA-CASIA data
+
+print("Loaded ASD / Hyperion data.\n")
+  #PRISMA data
+prisma_data = pd.read_csv(r'/LPDAAC/prisma_data.csv',
+                     low_memory=False, index_col=False, na_values='NaN')
+
+print("Loaded PRISMA data.\n")
+  #DESIS data
+desis_data = pd.read_csv(r'/LPDAAC/desis_data.csv', 
+                     low_memory=False, index_col=False, na_values='NaN')
+
+print("Loaded DESIS data.\n")
+
+#Dict for renaming all the prisma/desis columns.
+to_rename = {'JD' : 'JulianDay',
+             'Longitude' : 'Long',
+             'Latitude' : 'Lat',
+             'Crop Type' : 'Crop',
+             'GAEZ' : 'AEZ',
+             'Image ID' : 'Image',
+             'Unique ID' : 'UniqueID'
+             }
+
+    #Rename each column properly
+core_data.rename(columns={'jd' : 'JulianDay'}, inplace=True)
+core_data.rename(columns={"long" : "Long", "lat" : "Lat"}, inplace=True)
+desis_data.rename(columns=to_rename, inplace = True)
+prisma_data.rename(columns=to_rename, inplace = True)
+
+core_data.rename(columns={"long" : "Long", "lat" : "Lat"}, inplace=True)
+
+    #Convert month number to name
+month_labels = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May',
+                6: 'June', 7: 'July', 8: 'August',
+                9: 'September', 10: 'October', 11: 'November', 12: 'December'}
+
+core_data['Month'] = core_data['Month'].astype(int);
+core_data['Month'] = core_data['Month'].apply(lambda x: month_labels[x])
+    #Add sensor data for Hyperion data
+core_data.insert(0, "Sensor", 'Hyperion')
+
+    #Merge prisma and desis, capitalize months
+desis_data = desis_data.merge(prisma_data, how='outer')
+desis_data['Month'] = desis_data['Month'].str.capitalize()
+print(desis_data)
+
+    #Merge prisma/desis and hyperion
+core_data = core_data.merge(desis_data, how='outer')
+
+# Formatting
+core_data['Month'] = core_data['Month'].replace(['NA'], 0)
+core_data['Month'] = core_data['Month'].fillna(0)
+
+core_data['JulianDay'] = core_data['JulianDay'].replace(['NA'], 0)
+core_data['JulianDay'] = core_data['JulianDay'].fillna(0)
+
+core_data['Crop'] = core_data['Crop'].str.capitalize()
+
+core_data['Year'] = core_data['Year'].replace(['NA'], 2011)
+core_data['Year'] = core_data['Year'].fillna(2011)
+core_data['Year'] = core_data['Year'].astype(int);
+
+core_data['Sensor'] = core_data['Sensor'].replace(['ASD'], 'ASD Spectroradiometer')
+
+
+core_data = core_data.convert_dtypes()
+
+#Give a master ID to the whole dataframe
+core_data.insert(0, 'MasterID', range(0, 0 + len(core_data)))
+
+#Get the names of all the columns to unpivot
+names = []
+for i, c in core_data.items():
+    if c.name[0] == 'X':
+        names.append(c.name)
+
+#Unpivot columns
+data_flipped = core_data.melt(value_vars=names, id_vars='MasterID', var_name='Wavelength', value_name='Reflectance')
+
+#Get rid of the redundant wavelength columns
+core_data = core_data.drop(labels=names, axis='columns')
+
+#Merge the pivoted frame with the original frame
+data_flipped = pd.merge(core_data, data_flipped)
+core_data = data_flipped
+
+#Get rid of the leading 'X' and convert all the number strings to ints
+core_data['Wavelength'] = core_data['Wavelength'].map(lambda x: x.lstrip('X'))
+core_data['Wavelength'] = core_data['Wavelength'].astype(float)
+
+print("All data ready.")
+print(core_data)
+core_data.to_csv("core_data-long.csv", index = False)
